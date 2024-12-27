3895eb13fdfee2c400c876fad49304d91c0bb392
Dilki687 <chamodidilki44@gmail.com>
2024-12-27 11:21:15 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" contenet="width=device-width, initial-scale=1.0">
+        <title>about</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+
+    <body>
+        <section class="header">
+            <a href="home.php" class="logo">Triply</a>
+
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="about.php">about</a>
+                <a href="package.php">package</a>
+                <a href="book.php">book</a>
+            </nav>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+        </section>
+
+    <div class="heading" style="background:url(images/heading-img-1.jpg) no repeat">
+        <h1>about us</h1>
+    </div>
+
+    <!-- about section starts -->
+    
+    <section class="about">
+
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Make progress faster with our AI Learning Assistant, a tool that automatically understands your current course, instructions, and solution code — and gives you instant personalized feedback and guidance.</p>
+            <p>Make progress faster with our AI Learning Assistant, a tool that automatically understands your current course, instructions, and solution code — and gives you instant personalized feedback and guidance.</p>
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destinations</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 guide service</span>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- about section ends -->
+
+    <!-- reviews section starts -->
+
+    <section class="reviews">
+
+        <div class="swiper reviews-slider">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Find the right instructor for you. Choose from many topics, skill levels, and languages. Shop thousands of high-quality on-demand online courses. Start learning today. 30-Day Money Guarantee. ChatGPT AI Course. Learn ChatGPT. Download To Your Phone.</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-1.png" alt="">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Udemy is an online learning and teaching marketplace with over 220000 courses and 70 million students. Learn programming, marketing, data science and more</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-2.png" alt="">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Udemy is an online learning and teaching marketplace with over 220000 courses and 70 million students. Learn programming, marketing, data science and more</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-3.png" alt="">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Udemy is an online learning and teaching marketplace with over 220000 courses and 70 million students. Learn programming, marketing, data science and more</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-4.png" alt="">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Udemy is an online learning and teaching marketplace with over 220000 courses and 70 million students. Learn programming, marketing, data science and more</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-5.png" alt="">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="starts">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Udemy is an online learning and teaching marketplace with over 220000 courses and 70 million students. Learn programming, marketing, data science and more</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-6.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- reviews section ends -->
+
+    <!-- footer section starts -->
+     <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of user</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+                <a href="#"> <i class="fas fa-phone"></i> +003-776-0850</a>
+                <a href="#"> <i class="fas fa-envelope"></i> hello@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400567</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit"> created by <span> Chamodi Dilki</span> | all rights reserved! </div>
+
+     </section>
+
+    <!-- footer section ends -->
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
+
+    </body>
+
+</html>
+
---
+++ home.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" contenet="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+
+    <body>
+        <section class="header">
+            <a href="home.php" class="logo">Triply</a>
+
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="about.php">about</a>
+                <a href="package.php">package</a>
+                <a href="book.php">book</a>
+            </nav>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+        </section>
+
+        <!-- home section starts -->
+        <section class="home">
+
+            <div class="swiper home-slider">
+
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+
+                        <div class="content">
+                            <span>explore, discover, travel</span>
+                            <h3>travel around the world</h3>
+                            <a href="package.php" class="btn">discover more</a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+
+                        <div class="content">
+                            <span>explore, discover, travel</span>
+                            <h3>discover the new places</h3>
+                            <a href="package.php" class="btn">discover more</a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+
+                        <div class="content">
+                            <span>explore, discover, travel</span>
+                            <h3>make your tour worthwhile</h3>
+                            <a href="package.php" class="btn">discover more</a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+
+            </div>
+
+        </section>
+
+         <!-- home section ends -->
+
+
+        <!-- services section starts -->
+        <section class="services">
+            <h1 class="heading-title"> our services </h1>
+
+            <div class="box-container">
+
+                <div class="box">
+                    <img src="images/icon-1.png" alt="">
+                    <h3> adventure </h3>
+                </div>
+
+                <div class="box">
+                    <img src="images/icon-2.png" alt="">
+                    <h3> tour guide </h3>
+                </div>
+
+                <div class="box">
+                    <img src="images/icon-3.png" alt="">
+                    <h3> trekking </h3>
+                </div>
+
+                <div class="box">
+                    <img src="images/icon-4.png" alt="">
+                    <h3> camp fire </h3>
+                </div>
+
+                <div class="box">
+                    <img src="images/icon-5.png" alt="">
+                    <h3> off road </h3>
+                </div>
+
+                <div class="box">
+                    <img src="images/icon-6.png" alt="">
+                    <h3> camping </h3>
+                </div>
+
+            </div>
+
+        </section>
+
+         <!-- services section ends -->
+
+        <!-- home about section starts -->
+
+        <section class="home-about">
+
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>about us</h3>
+            <p>Kotlin is a programming language that was released in 2011 by JetBrains, a company that sells integrated development environments (IDEs) for programming languages. Since then, it’s become a favorite language for developers and replaced Java in many software projects.</p>
+            <a href="about.php" class="btn">read more</a>
+        </div>
+
+        </section>
+
+        <!-- home about section ends -->
+
+        <!-- home packages section starts -->
+        
+        <section class="home-packages">
+
+            <h1 class="heading-title"> our packages </h1>
+
+            <div class="box-container">
+
+                <div class="box">
+
+                    <div class="image">
+                    <img src="images/img-1.jpg" alt="">
+                    </div>
+
+                    <div class="content">
+                        <h3>adventure & tour</h3>
+                        <p>Join the millions learning to code with Codecademy for free</p>
+                        <a href="book.php" class="btn">book now</a>
+                    </div>
+
+                </div>
+
+                <div class="box">
+
+                    <div class="image">
+                    <img src="images/img-2.jpg" alt="">
+                    </div>
+
+                    <div class="content">
+                        <h3>adventure & tour</h3>
+                        <p>Join the millions learning to code with Codecademy for free</p>
+                        <a href="book.php" class="btn">book now</a>
+                    </div>
+
+                </div>
+
+                <div class="box">
+
+                    <div class="image">
+                    <img src="images/img-3.jpg" alt="">
+                    </div>
+
+                    <div class="content">
+                        <h3>adventure & tour</h3>
+                        <p>Join the millions learning to code with Codecademy for free</p>
+                        <a href="book.php" class="btn">book now</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+
+        </section>
+
+        <!-- home packages section ends -->
+
+        
+
+        <!-- home offer sectio starts -->
+
+        <section class="home-offer">
+            <div class="content">
+                <h3>upto 50% offer</h3>
+                <p>Make progress faster with our AI Learning Assistant, a tool that automatically understands your current course, instructions, and solution code — and gives you instant personalized feedback and guidance.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </section>
+
+        <!-- home offer section ends -->
+
+
+        
+    <!-- footer section starts -->
+     <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of user</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+                <a href="#"> <i class="fas fa-phone"></i> +003-776-0850</a>
+                <a href="#"> <i class="fas fa-envelope"></i> hello@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400567</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit"> created by <span> Chamodi Dilki</span> | all rights reserved! </div>
+
+     </section>
+
+    <!-- footer section ends -->
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
+
+    </body>
+
+</html>
+
